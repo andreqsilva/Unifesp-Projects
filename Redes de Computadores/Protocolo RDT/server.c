671bc6e546022fa911ac719dbcc1eca671ebe967
andreqsilva <andrefernandoqsilva31@gmail.com>
2022-12-18 18:40:30 -0300

    update

--- Redes de Computadores/Protocolo RDT/server.c
+++ Redes de Computadores/Protocolo RDT/server.c
@@ -3,47 +3,45 @@
 
 int main(int argc, char **argv) {
 
-	if (argc != 2) {
-			printf("%s <porta>\n", argv[0]);
-			return 0;
-	}
+  if (argc != 2) {
+    printf("%s <porta>\n", argv[0]);
+    return 0;
+  }
 
-	int s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	if (s < 0) {
-		perror("socket()");
-		return -1;
-	}
+  int s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if (s < 0) {
+    perror("socket()");
+    return -1;
+  }
 
-	struct sockaddr_in addr;
-	addr.sin_addr.s_addr = INADDR_ANY;
-	addr.sin_port = htons(atoi(argv[1]));
-	addr.sin_family = AF_INET;
+  struct sockaddr_in addr;
+  addr.sin_addr.s_addr = INADDR_ANY;
+  addr.sin_port = htons(atoi(argv[1]));
+  addr.sin_family = AF_INET;
 
-	if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-		perror("bind()");
-		return -1;
-	}
+  if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+    perror("bind()");
+    return -1;
+  }
 
-	unsigned char message[MAX_REQ] = {"packet"};
-	int cfd, nr, msg_size = sizeof(message);
-	socklen_t addr_len;
+  unsigned char message[MAX_REQ] = {"packet"};
+  int cfd, nr, msg_size = sizeof(message);
+  socklen_t addr_len;
 
-	while (1) {
-		struct sockaddr_in caddr;
-		addr_len = sizeof(struct sockaddr_in);
-		bzero(&caddr, addr_len);
-		if (cfd < 0) {
-			perror("accept()");
-			close(s);
-			return -1;
-		}
-		cfd = s;
-		unsigned char req[MAX_REQ];
-		nr = recvfrom(cfd, req, MAX_REQ, 0, (struct sockaddr *)&caddr, &addr_len);
-		rdt_send(cfd, message, msg_size, caddr);
-	}
+  while (1) {
+    struct sockaddr_in caddr;
+    addr_len = sizeof(struct sockaddr_in);
+    if (cfd < 0) {
+      perror("accept()");
+      close(s);
+      return -1;
+    }
+    cfd = s;
+    unsigned char req[MAX_RES];
+    nr = recvfrom(cfd, req, MAX_REQ, 0, (struct sockaddr *)&caddr, &addr_len);
+    rdt_send(cfd, message, msg_size, caddr);
+  }
 
-	close(s);
-	return 0;
+  close(s);
+  return 0;
 }
-
